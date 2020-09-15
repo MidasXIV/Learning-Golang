3e96d2c4ee47805de421753a2d87213b37b83d07
MidasXIV <amanshaikh.arc@gmail.com>
2020-09-16 01:47:55 +0400

    Grouping variable declaration and initialization

--- src/github.com/midasxiv/variables/main.go
+++ src/github.com/midasxiv/variables/main.go
@@ -4,27 +4,26 @@ import "fmt"
 
 func main() {
 	fmt.Println("Learning Variables")
-	// var<keyword> <variable-name> <variable-type>
-	var i int
 
-	// initializing a variable
-	i = 42
-	i = 27 // variables can be reassigned
+	// Type                       -> Zero Value
+	// bool                       -> false
+	// all numeric types          -> 0
+	// single character           -> 	0
+	// string                     -> ""
+	// array                      -> array of proper length where all elements have their zero value
+	// slice                      -> empty slice with length 0 and capacity 0
+	// map                        -> empty map
+	// struct                     -> struct where all fields have their zero value
+	// pointers & all other types -> nil
 
-	// declaring a variable and assigning a value with type
-	var j float32 = 32
+	var firstName, age = "Mark", 57 // must initialize all
+	var (                           // use parens to spread over multiple lines
+		lastName string = "Twain"
+		books    int    // not initialized, so uses zero value
+	)
 
-	// declaring a variable and assigning a value
-	// but let Go infer the type
-	k := 34.0
-	// Go infers interger to be of type int or float64
+	// all variables declared must be used else go gives an error
+	fmt.Printf("%v %v | Age - %v | Books - %v", firstName, lastName, age, books)
 
-	fmt.Println(i, j, k)
-
-	// print using formatted string
-	// %v is used to print value
-	// %T is used to print type
-	fmt.Printf("%v, %T\n", i, i)
-	fmt.Printf("%v, %T\n", j, j)
-	fmt.Printf("%v, %T\n", k, k)
+	// no private variables
 }
